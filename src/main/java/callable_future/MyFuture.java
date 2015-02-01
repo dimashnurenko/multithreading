cc40f80b50a170808bd2c5daa9259a3414707d95
Dmitry Shnurenko <dshnurenko@codenvy.com>
2015-02-01 17:34:05 +0200

    callable, executor, future

---
+++ src/main/java/callable_future/MyFuture.java
@@ -0,0 +1,40 @@
+package callable_future;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * Created by Шнуренко on 01.02.2015.
+ */
+public class MyFuture implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(1000);
+
+        return Thread.currentThread().getName();
+    }
+
+    public static void main(String args[]) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        List<Future<String>> list = new ArrayList<>();
+
+        Callable<String> callable = new MyFuture();
+        for (int i = 0; i < 100; i++) {
+
+            Future<String> future = executor.submit(callable);
+
+            list.add(future);
+        }
+        for (Future<String> fut : list) {
+            try {
+                System.out.println(new Date() + "::" + fut.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
+    }
+}
